957c6b2a3c7fd9591de9ed9ac03dbcec2c2f2c59
Karl <spybug@users.noreply.github.com>
2017-08-08 16:56:13 -0700

    Gets the location of the device (bugged)

--- handlers.js
+++ handlers.js
@@ -1,8 +1,9 @@
 'use strict';
 var moment = require('moment');
-const ASK = ':ask'
-const TELL = ':tell'
-const UTC_SSML = "<say-as interpret-as='spell-out'>utc</say-as>"
+//var https = require('https');
+const ASK = ':ask';
+const TELL = ':tell';
+const UTC_SSML = "<say-as interpret-as='spell-out'>utc</say-as>";
 
 module.exports = {
 
@@ -16,14 +17,24 @@ module.exports = {
 		var datestr = timestamp.format("MMDD");
 		var timestr = timestamp.format("HH:mm");
 		var datessml = `<say-as interpret-as='date'>????${datestr}</say-as>`;
-		console.log(`date: ${datestr}\t time: ${timestr}`);
-		console.log(datessml);
 		
 		var result = `The current ${UTC_SSML} time is: ${timestr}, on ${datessml}`;
-		console.log(result);
 		
         this.emit(TELL, result);
-    },
+	},
+	
+	"GetLocalOffset": function() {
+		try {
+			var deviceId = this.event.context.System.device.deviceId;
+			var token = this.event.context.System.user.permissions.consentToken;
+			alexa_location = module.exports.getAlexaLocation(deviceId, token);
+			lat_lon = module.exports.getLatLon(alexa_location.countryCode, alexa_location.postalCode);
+		}
+		catch(e) {
+			this.emit(TELL, "Error getting local time from device");
+			console.log(e)
+		}
+	},
 	
 	"AMAZON.HelpIntent": function() {
 		this.emit(ASK, `Please ask for the current time in ${UTC_SSML}`);
@@ -35,5 +46,31 @@ module.exports = {
 	
 	"AMAZON.StopIntent": function() {
 		this.emit(TELL, 'Goodbye.');
+	},
+
+	getAlexaLocation : function(deviceId, token) {
+		var alexa_api = `https://api.amazonalexa.com/v1/devices/${deviceId}/settings/address/countryAndPostalCode`
+		var alexa_headers = {'Accept': 'application/json', 'Authorization': `Bearer ${token}`}
+		var options = {hostname: alexa_api, headers: alexa_headers, method: 'GET'};
+		console.log(options);
+		https.get(options, (res) => {
+			var body = '';
+			res.on('data', (data) => {
+				body += data;
+			});
+			res.on('end', () => {
+				var data = JSON.parse(body);
+				console.log(data);
+				return { 
+					countryCode: data.countryCode,
+					postalCode: data.postalCode
+				};
+			});
+		})
+	}, 
+
+	getLatLon : function(countryCode, postalCode) {
+		console.log(countryCode);
+		console.log(postalCode);
 	}
 };
--- index.js
+++ index.js
@@ -3,14 +3,16 @@ var Alexa = require('alexa-sdk');
 var handlers = require('./handlers');
 
 const APP_ID =  process.env['APP_ID'];
-const ASK = ':ask'
-const TELL = ':tell'
+const ASK = ':ask';
+const TELL = ':tell';
 
 exports.handler = function(event, context, callback) {
 	// fix broken skill simulator json
 	if (event.context && event.context.System) {
 		event.context.System.application.applicationId = event.session.application.applicationId;
 		event.context.System.user.userId = event.session.user.userId;
+		event.context.System.user.permissions = event.session.user.permissions;
+		event.context.System.device = event.session.device;
 	}
     var alexa = Alexa.handler(event, context, callback);
 	
